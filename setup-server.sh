0c03e57177cd75f2e7d18e47098cd0451b41c26a
Arpit Agarwal <ravaan@users.noreply.github.com>
2023-05-24 21:56:51 +0000

    add setup server script for ubuntu

---
+++ setup-server.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo npm i -g pm2
+sudo apt update
+sudo apt install nginx
+pub_key="<YOUR_KEY_GOES_HERE>"
+echo $pub_key >> ~/.ssh/id_rsa.pub
